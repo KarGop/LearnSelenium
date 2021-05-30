8b0a28a2049c89b3a3485d49a481823065a64e68
KarGop <66565340+KarGop@users.noreply.github.com>
2021-05-30 13:58:14 +0530

    TestNG Annotations order

---
+++ TestNG/learnAnnotations/LTTestCase.java
@@ -0,0 +1,21 @@
+package testNG.learnAnnotations;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LTTestCase extends BaseClass{
+	
+	@Test
+	  public void test2() {
+		  
+		  System.out.println("test2");
+		  
+	  }
+	
+	@BeforeMethod
+	public void preCondition() {
+		System.out.println("preCondition");
+
+	}
+
+}
---
+++ TestNG/learnAnnotations/TestCase.java
@@ -0,0 +1,12 @@
+package testNG.learnAnnotations;
+
+import org.testng.annotations.Test;
+
+public class TestCase extends BaseClass{
+ 
+  @Test
+  public void test1() {
+	  
+	  System.out.println("test1");
+	  
+  }}
